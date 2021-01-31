c0ef8c942355f6ee6b84dac93fd198308dba458a
Myron <dubtwee@gmail.com>
2021-01-30 22:51:26 -0600

    creating a secrets page for displaying all of the secrets from the server

---
+++ src/components/SecretsPage/SecretsPage.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import useReduxStore from '../../hooks/useReduxStore';
+
+function SecretsPage() {
+  const dispatch = useDispatch();
+  const store = useReduxStore();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_SECRETS' });
+  }, [dispatch]);
+
+  return (
+    <div>
+      <p>
+        Currently logged in as <b>{store.user.username}</b>
+      </p>
+      <p>
+        Clearance level: <b>{store.user.clearance_level}</b>
+      </p>
+      <ul>
+        {store.secrets.map((secret) => (
+          <li>
+            Clearance: {secret.secrecy_level} | Content: {secret.content}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SecretsPage;

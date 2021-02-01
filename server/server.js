cd67d20f6548bfbda80f4fc7129ed777608b9ab8
Myron <dubtwee@gmail.com>
2021-01-31 21:47:21 -0600

    adding the base stubbed in server route for the shelf

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const shelfRouter = require('./routes/shelf.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/shelf', shelfRouter);
 
 // Serve static files
 app.use(express.static('build'));

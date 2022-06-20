a40ae42c7a0c1d8809e582250aea2fd5dc3e5b02
Brian Campbell <bry123ftw@gmail.com>
2022-06-20 17:02:37 -0500

    post close to working

--- server/routes/runs.router.js
+++ server/routes/runs.router.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+    rejectUnauthenticated,
+  } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log("req.user is:", req.user);
     pool
         .query(`SELECT * FROM "runs";`)
         .then((results)=> res.send(results.rows))
         .catch((error) => {
             console.log('Error making SELECT for runs:', error);
+            res.sendStatus(500);
+          });
+});
+
+/* category ids:
+    1: speed
+    2: long
+    3: fun
+    4: causal/social
+    5: race
+*/
+
+// note: issue with postman logging in... 
+
+router.post('/', rejectUnauthenticated, (req, res) => {
+    console.log("req.user is:", req.user);
+    const query = `INSERT INTO "runs" ("user_id", "distance", "time", "pace", "cat_id", "date", "notes")
+    VALUES ($1, $2, $3, $4, $5, $6, $7);`
+    pool
+        .query(query, [req.user.id, distance, time, pace, cat_id, date, notes])
+        .then((results)=> res.send(results.rows))
+        .catch((error) => {
+            console.log('Error making Posting to runs:', error);
             res.sendStatus(500);
           });
 });

0f9d4605c9b7049b3618befe857bb241fb221ab2
Brian Campbell <bry123ftw@gmail.com>
2022-06-21 11:14:00 -0500

    delete function works

--- server/routes/runs.router.js
+++ server/routes/runs.router.js
@@ -3,7 +3,7 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const {
     rejectUnauthenticated,
-  } = require('../modules/authentication-middleware');
+} = require('../modules/authentication-middleware');
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log("req.user is:", req.user);
@@ -12,11 +12,11 @@ router.get('/', rejectUnauthenticated, (req, res) => {
         .query(`SELECT * FROM "runs" 
         JOIN "user" ON runs.user_id = "user"."id"
         where "user".id = ${req.user.id};`)
-        .then((results)=> res.send(results.rows))
+        .then((results) => res.send(results.rows))
         .catch((error) => {
             console.log('Error making SELECT for runs:', error);
             res.sendStatus(500);
-          });
+        });
 });
 
 /* category ids:
@@ -35,13 +35,23 @@ router.post('/', rejectUnauthenticated, (req, res) => {
     VALUES ($1, $2, $3, $4, $5, $6, $7);`
     pool
         .query(query, [req.user.id, req.body.distance, req.body.time, req.body.pace, req.body.cat_id, req.body.date, req.body.notes])
-        .then((results)=> res.send(results.rows))
+        .then((results) => res.send(results.rows))
         .catch((error) => {
             console.log('Error making Posting to runs:', error);
             res.sendStatus(500);
-          });
+        });
 });
 
-
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+    console.log('in delete', req.params, req.user.id)
+    const query = `DELETE FROM runs WHERE runs.id = $1 AND runs.user_id = $2;`
+    pool
+        .query(query, [req.params.id, req.user.id])
+        .then((response) => res.sendStatus(204))
+        .catch(error => {
+            console.log(`Error deleting run`, error);
+            res.sendStatus(500);
+        });
+})
 
 module.exports = router;
